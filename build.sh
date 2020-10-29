679fe24b65e7bcf115b33a53c53833d7e9b22aee
Francisco Ares <francisco.ares@easytech.com.br>
2020-10-29 14:05:19 -0300

    Minor: Improve build script

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 if [ -e Makefile ]
 then
+    make clean -j4
     make distclean
 fi
-qmake menu.pro -spec linux-g++-64 "CONFIG-=debug release debug_and_release" "CONFIG+=release" && make qmake_all
-# make  $MAKEOPTS && strip menu && upx --best --ultra-brute menu && A=`tempfile` && rm -f $A ; mkdir $A && mv menu $A && make distclean && mv $A/menu .
-make -s $MAKEOPTS && strip menu &&                                  A=`tempfile` && rm -f $A ;          && mv menu $A && make distclean && mv $A menu && rm -fR GeneratedFiles/ release/ debug/
+QT_OTIMIZA="linux-g++"
+MAQUINA=`uname -m`
+lrelease iView.pro 2> /dev/null
+if [ "$MAQUINA" = "x86_64" ]
+then
+        QT_OTIMIZA="linux-g++-64"
+fi
+qmake menu.pro -spec $QT_OTIMIZA "CONFIG-=debug release debug_and_release" "CONFIG+=release" && make qmake_all 1> /dev/null 2> /dev/null
+# make  $MAKEOPTS && strip release/menu && upx --best --ultra-brute menu && A=`tempfile` && rm -f $A ; mkdir $A && mv release/menu $A && make distclean && mv $A/menu .
+make -s $MAKEOPTS && strip release/menu &&                                  A=`tempfile` && rm -f $A            && mv release/menu $A && make distclean && mv $A menu && rm -fR GeneratedFiles/ release/ debug/
